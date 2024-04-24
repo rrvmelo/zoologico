1c1e23993582f6c95af055642bbe2bee7f9e616b
vboxuser <rrvmelo@gmail.com>
2024-04-24 17:14:36 -0300

    Aula 19/14 Twig inicial

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,16 +2,34 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use function Symfony\Component\string\u;
 
-class HelloController 
+class HelloController extends AbstractController
 {
     #[Route('/')]
     public function index(): Response
     {
-        return new Response('Olá, Mundo!');
+
+        $users = [
+            ['name' => 'Filip Doe', 'phone' => '(19) 93313-2406' ],
+            ['name' => 'Billy Doe', 'phone' => '(19) 93313-2406' ],
+            ['name' => 'Charles Doe', 'phone' => '(19) 93313-2406' ],
+            ['name' => 'John Doe', 'phone' => '(19) 92213-2406' ],
+            ['name' => 'Emma Doe', 'phone' => '(19) 93313-9999' ],
+            ['name' => 'Elle Doe', 'phone' => '(19) 93313-2406' ],
+            ['name' => 'Rohn Doe', 'phone' => '(19) 93313-7777' ],
+        ];
+
+   
+
+
+        return $this->render('hello/homepage.html.twig',[
+            'title' => 'Zoológico',
+            'users' => $users,
+        ]);
     }
 
     #[Route('/animal/{slug}')]
